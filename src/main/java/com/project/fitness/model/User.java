6474662f89e8e6250f59e4bcbee66e054d1d5dc6
Harsh <harshofficial1905@gmail.com>
2026-04-12 13:59:37 +0530

    Implemented User CRUD APIs with controller,service, and repository layers

--- src/main/java/com/project/fitness/model/User.java
+++ src/main/java/com/project/fitness/model/User.java
@@ -2,6 +2,7 @@ package com.project.fitness.model;
 
 import jakarta.persistence.*;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "users")
@@ -18,15 +19,51 @@ public class User {
 
     private String password;
 
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    @JsonIgnore
     @OneToMany(mappedBy = "user")
     private List<Workout> workouts;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "user")
     private List<Meal> meals;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "user")
     private List<Goal> goals;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "user")
     private List<WeightLog> weightLogs;
 
